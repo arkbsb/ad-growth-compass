f4c57c16de4f4790d7c947925b94b24943aa342f
Junior Maia <juniormaia@192.168.1.215>
2025-07-22 11:25:51 -0300

    Update package-lock.json with serve dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -10,10 +8,10 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          {/* Logo removido temporariamente */}
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          {/* Logo removido temporariamente */}
         </a>
       </div>
       <h1>Vite + React</h1>
